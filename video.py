bd53747dd4b224d67a529f18b1ec9e8fa0ec14b1
Antonio Gonzalez <antgon@cantab.net>
2018-10-01 14:26:23 +0100

    Added a new class for reading multi-frame tiffs.

--- video.py
+++ video.py
@@ -18,12 +18,134 @@
 # along with videoroi. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-import numpy as np
+import os
+import warnings
 
 # Requires OpenCV 3
 import cv2
 cv2_ver = cv2.__version__.split('.')
 assert int(cv2_ver[0]) >= 3
+
+# Optional: tiffffile to manage tif videos (i.e. multi-image tiffs)
+try:
+    from contrib import tifffile
+    TIFF_SUPPORT = True
+except ModuleNotFoundError:
+    TIFF_SUPPORT = False
+
+
+class VideoTiff:
+    '''
+    A class for reading a multi-frame tiff. Requires Christoph Gohlke's
+    tifffile.py, found here:
+    https://www.lfd.uci.edu/~gohlke/code/tifffile.py.html
+    '''
+    def __init__(self, filename, fps=None):
+        self.filename = filename
+        if TIFF_SUPPORT is False:
+            raise ModuleNotFoundError("Requires tifffile.py")
+        self.capture = tifffile.imread(filename)
+        self._width = self.capture.shape[-1]
+        self._height = self.capture.shape[-2]
+        self._frame_count = self.capture.shape[-3]
+        if fps is None:
+            warnings.warn("FPS is not defined, defaulting to 1.")
+            fps = 1
+        self._fps = fps
+        self._current_frame = 0
+        # An alternative implementation:
+        # t = tifffile.TiffFile(filename)
+        # page = t.pages[0]
+        # page.imagelength, page.imagewidth
+        # frame_count = len(t.pages)
+        # im = t.pages[9].asarray()
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def frame_count(self):
+        return self._frame_count
+
+    @property
+    def fps(self):
+        return self._fps
+
+    @fps.setter
+    def fps(self, value):
+        self._fps = value
+
+    @property
+    def duration(self):
+        return self.frame_count / self.fps
+
+    def seek_frame(self, frame_number=0):
+        '''
+        Moves the pointer to frame `frame_number` (0-based index). If
+        `frame_number` is greater than the total number of frames, then the
+        pointer will be the last frame in the video.
+        '''
+        if frame_number >= self.frame_count:
+            print("End of file reached.")
+            frame_number = self.frame_count - 1
+        self._current_frame = frame_number
+
+    def seek_time(self, milliseconds=0):
+        '''
+        Moves the pointer to the frame that is closest in time to
+        `milliseconds`.
+        '''
+        seconds = milliseconds/1000
+        frame = round(seconds * self.fps)
+        self.seek_frame(frame)
+
+    def read(self, frame_number=None):
+        '''
+        Returns video frame `frame_number` (0-based index). If no frame_number
+        is specified, the frame returned will be
+        '''
+        if frame_number is not None:
+            # If a specific frame has been requested move the pointer to that
+            # frame before reading data.
+            self.seek_frame(frame_number)
+        # Read the video frame.
+        img = self.capture[self.pos_frames, :, :]
+        # After reading the frame shift the pointer one place forward so that
+        # the next read will return the next frame in the video.
+        self.seek_frame(self.pos_frames + 1)
+        return img
+
+    @property
+    def pos_frames(self):
+        return self._current_frame
+
+    @property
+    def pos_ms(self):
+        '''
+        Position in milliseconds
+        '''
+        return self._current_frame / self.fps * 1000
+
+    @property
+    def duration_str(self):
+        '''
+        Returns total duration in string format min:sec
+        '''
+        if self.duration is None:
+            return ''
+        else:
+            m = self.duration // 60.
+            s = self.duration % 60.
+            return '{:02}:{:02}'.format(int(m), int(s))
+
+    def close(self):
+        # self.capture.release()
+        pass
 
 
 class Video:
@@ -106,3 +228,9 @@ class Video:
 
     def close(self):
         self.capture.release()
+
+
+if __name__ == "__main__":
+    fname = os.path.expanduser("~/projects/photometry/glucose_in_vitro/" +
+                               "data/run1_Registered.tif")
+    vid = VideoTiff(fname)
